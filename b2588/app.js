62133450917d5f520ce9f44c20ffa2a1c0b698a3
sori-sodong <89089430+sori-sodong@users.noreply.github.com>
2021-08-22 22:55:44 +0900

    Delete b2588 directory

--- b2588/app.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const filePath = process.platform === 'linux' ? '/dev/stdin' : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split('/n');
-
-const firstNum = input[0];
-const secondNum = input[1];
-
-console.log(secondNum);
-
